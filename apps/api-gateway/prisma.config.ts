1b838317b6dc8bda1d5756e14e09dde7c31d7342
Maxim Farizov <farnaks.net@gmail.com>
2026-08-27 11:30:35 +0300

    fix seed

--- apps/api-gateway/prisma.config.ts
+++ apps/api-gateway/prisma.config.ts
@@ -6,9 +6,10 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
+    seed: 'npx ts-node prisma/seed.ts',
     path: "prisma/migrations",
   },
   datasource: {
     url: process.env["DATABASE_URL"],
-  },
+  }
 });
